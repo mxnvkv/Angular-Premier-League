935cb8be2974b24ace384347e4d4e01939a1631d
mxnvkv <38319843+mxnvkv@users.noreply.github.com>
2020-04-14 16:11:01 +0300

    initial-commit

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['app.component.scss'],
+  template: `
+    <div>
+      Angular Premier League project works!
+    </div>
+  `
+})
+
+export class AppComponent {
+
+}
